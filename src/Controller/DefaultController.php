af9e08ae8a5b6ad22894dcb72a9b6319e465fbce
Joe Daigle <josephldaigle@yahoo.com>
2019-02-24 17:59:50 -0500

    add encore webpack and update page templates

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,14 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * DefaultController.
+ *
+ * Provides the home page.
+ *
+ * @package App\Controller
+ */
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/default", name="default")
+     * @Route("/", name="home")
      */
     public function index()
     {
-        return $this->render('default/index.html.twig', [
+        return $this->render('default/maintenance.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
     }
