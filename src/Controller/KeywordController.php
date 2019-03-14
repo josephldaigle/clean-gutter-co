957d077bb9686693f797f81079d9767b687d8780
Joe Daigle <josephldaigle@yahoo.com>
2019-03-13 23:06:46 -0400

    start working on city pages.

---
+++ src/Controller/KeywordController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by Joseph Daigle.
+ * Date: 3/13/19
+ * Time: 8:47 PM
+ */
+
+namespace CleanGutter\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * KeywordController.
+ *
+ * @package CleanGutter\Controller
+ */
+class KeywordController
+{
+//	public function getLocationKeyword( Request $request )
+//	{
+//
+//	}
+}
