17967bcc814e37ee7aeaa2011553bd6cc394fad4
Joe Daigle <josephldaigle@yahoo.com>
2019-04-02 21:20:38 -0400

    update templates

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,7 +5,6 @@ namespace CleanGutter\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-//use Symfony\Component\Routing\RouterInterface;
 
 /**
  * DefaultController.
