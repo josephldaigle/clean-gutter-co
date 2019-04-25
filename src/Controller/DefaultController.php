5c21c072339ff30e4d5994f59105de2533ecb2b2
Joe Daigle <josephldaigle@yahoo.com>
2019-04-24 21:25:21 -0400

    add faq, tos and privacy pages

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,7 +4,6 @@ namespace CleanGutter\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * DefaultController.
@@ -19,6 +18,7 @@ class DefaultController extends AbstractController
 	 * @param Request $request
 	 *
 	 * @return \Symfony\Component\HttpFoundation\Response
+	 * @throws \LogicException
 	 */
 	public function getHome(Request $request)
 	{
@@ -29,6 +29,7 @@ class DefaultController extends AbstractController
 
 	/**
 	 * @return \Symfony\Component\HttpFoundation\Response
+	 * @throws \LogicException
 	 */
 	public function getLandingPage()
 	{
@@ -37,16 +38,36 @@ class DefaultController extends AbstractController
 		]);
 	}
 
-//	/**
-//	 * @param Request $request
-//	 *
-//	 * @return \Symfony\Component\HttpFoundation\Response
-//	 */
-//    public function city(Request $request, RouterInterface $router)
-//    {
-//        return $this->render('page/city.html.twig', [
-//            'controller_name' => 'DefaultController',
-//	        'data' => ['customers' => ['count' => rand(1, 25)], 'city' => $request->getUri()]
-//        ]);
-//    }
+	/**
+	 * @param Request $request
+	 *
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 * @throws \LogicException
+	 */
+	public function getFaq(Request $request)
+	{
+		$questions = [
+			[
+				'question' => 'Is the vacuum really powerful enough to clean my gutters?',
+				'answer' => 'YES! We wouldn\'t waste your time. Our vacuum uses those big plugs (240v) like your dryer, and has three motors. It can lift 12\'\' saplings out of your gutters, and shreds through leaves like butter. You can see it in action here or check out the manufacturers product page to learn more.'
+			],
+			[
+				'question' => 'What kind of insurance do you have?',
+				'answer' => 'We carry a general liability policy from Hiscox Insurance Company, Inc. It covers any accidental damages to your home, and some bodily injury.'
+			],
+			[
+				'question' => 'Do I have to be home when you clean my gutters?',
+				'answer' => 'Not necessarily. As long as we can access all of your gutters, and you don\'t have any free-roaming animals, we should be able to complete the job without you being home. We\'ll email you the invoice, and you can pay online.'
+			],
+			[
+				'question' => 'How often should I have my gutters cleaned?',
+				'answer' => 'Generally, twice a year - in late autumn and early spring. You may need more or less frequent cleanings depending on how many trees there are around your home. We\'ll let you know once we\'ve had a chance to view your property first-hand, help you establish a regular cleaning schedule, so your gutters are able to do their job year-round.'
+			]
+		];
+
+		return $this->render('page/faq.html.twig', [
+			'questions' => $questions,
+			'controller_name' => 'DefaultController'
+		]);
+	}
 }
