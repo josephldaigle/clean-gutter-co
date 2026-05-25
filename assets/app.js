e8e931ca56445cd4002cf769888cced38a93e4d6
Joe Daigle <josephldaigle@yahoo.com>
2026-05-25 10:02:30 -0400

    finish front-end build fix

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,8 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import '../css/app.scss';
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import $ from 'jquery';
+window.$ = $;
+window.jQuery = $;
 
-// start the Stimulus application
-import './bootstrap';
+import 'popper.js';
+import 'bootstrap';
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,8 @@
 import '../css/app.scss';
+
+import $ from 'jquery';
+window.$ = $;
+window.jQuery = $;
+
+import 'popper.js';
+import 'bootstrap';
