ed776c5a3d0d361e8f262c570970e5f27aac122e
Joe Daigle <josephldaigle@yahoo.com>
2026-05-25 07:34:48 -0400

    Restore webpack image copying

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,11 @@ Encore
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
+    .copyFiles({
+        from: './assets/img',
+        to: 'images/[path][name].[ext]'
 
+    })
     /*
      * FEATURE CONFIG
      *
